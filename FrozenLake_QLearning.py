646b30da330ba42cf49e8a85adab3936620b6f8f
Jian Guo <guojian_k@qq.com>
2024-01-26 18:51:02 +0800

    q learning

--- FrozenLake_QLearning.py
+++ FrozenLake_QLearning.py
@@ -3,6 +3,9 @@ import gymnasium
 import numpy as np
 import random
 
+import torch
+from torch import nn
+
 
 class QLearningAgent:
     def __init__(self):
@@ -49,6 +52,35 @@ class QLearningAgent:
         if terminated and r != 1:
             r = -1
         self.q[s][a] = float(r) + self.gamma * max(self.q[next_s])
+        pass
+
+
+class QNetwork(torch.nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.model = nn.Sequential(
+            nn.Linear(4, 64),
+            nn.ReLU(),
+            nn.Linear(64, 4)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class QAgent(object):
+
+    def __init__(self):
+        self.memory = collections.deque(maxlen=1000)
+        self.lr = 0.001
+        self.q = QNetwork()
+        self.target_q = QNetwork()
+
+    def memory_update(self, s, a, r, next_s, terminated):
+        self.memory.append((s, a, r, next_s, terminated))
+
+    def update_q(self):
         pass
 
 
