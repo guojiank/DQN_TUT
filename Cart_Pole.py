0e1d0881a739e24ae4213c90e92481a100af7e77
Jian Guo <guojian_k@qq.com>
2024-02-04 17:44:00 +0800

    update

--- Cart_Pole.py
+++ Cart_Pole.py
@@ -5,7 +5,7 @@ import random
 import gymnasium
 import numpy as np
 import torch.optim
-from torch import nn, FloatTensor
+from torch import nn, FloatTensor, BoolTensor, LongTensor
 from tqdm import trange
 
 
@@ -31,8 +31,10 @@ class Memory:
     def __init__(self):
         self.q = deque([], maxlen=MEMORY_SIZE)
 
-    def push(self, state, action, reward, next_state, done):
-        self.q.append((state, action, reward, next_state, done))
+    def push(self, state, action, reward, next_state):
+        if next_state is not None:
+            next_state = FloatTensor(next_state)
+        self.q.append((FloatTensor(state), LongTensor([action]), FloatTensor([reward]), next_state))
 
     def sample(self, batch_size):
         return random.sample(self.q, batch_size)
@@ -47,9 +49,8 @@ class Agent:
         self.target_q_network = Net()
         self.loss_fn = nn.SmoothL1Loss()
         self.target_q_network.load_state_dict(self.q_network.state_dict())
-        self.optimizer = torch.optim.AdamW(self.q_network.parameters(), lr=0.0001)
+        self.optimizer = torch.optim.AdamW(self.q_network.parameters())
         self.counter = 0
-        pass
 
     def choose_action(self, state):
         e = 0.05 + (0.9 - 0.05) * math.exp(-1. * self.counter / 1000)
@@ -57,26 +58,27 @@ class Agent:
         if random.random() < e:
             return random.choice([0, 1])
         else:
-            return self.action(state)
-
-    def action(self, state):
-        return self.q_network(FloatTensor(np.array([state]))).argmax().item()
+            return self.q_network(FloatTensor(np.array([state]))).argmax().item()
 
     def train(self, m: Memory):
-        if m.size() < MEMORY_SIZE:
+        if m.size() < 128:
             return
-        arr = np.array(m.sample(128))
+        batch = list(zip(*m.sample(128)))
 
-        b_s = FloatTensor(arr[:, 1]).reshape(-1, 4)
-        b_a = FloatTensor(arr[:, 2]).reshape(-1, 1)
-        b_r = FloatTensor(arr[:, 3]).reshape(-1, 1)
-        b_s_ = FloatTensor(arr[:, 4]).reshape(-1, 4)
-        b_dones = FloatTensor(arr[:, 5]).reshape(-1, 1)
+        b_s = torch.cat(batch[0]).reshape(-1, 4)
+        b_a = torch.cat(batch[1]).reshape(-1, 1)
+        b_r = torch.cat(batch[2]).reshape(-1, 1)
+
+        not_none_next_s = torch.cat([i for i in batch[3] if i is not None]).reshape(-1, 4)
+        next_s_mask = torch.BoolTensor([[i is not None] for i in batch[3]])
 
         batch_action_values = self.q_network(b_s).gather(1, b_a)
-        next_batch_action_values = self.target_q_network(b_s_).max(1)[0].reshape(-1, 1)
 
-        targets = b_r + 0.99 * (1 - b_dones) * next_batch_action_values
+        next_batch_action_values = torch.zeros(128).reshape(-1, 1)
+
+        next_batch_action_values[next_s_mask] = self.target_q_network(not_none_next_s).max(1).values
+
+        targets = b_r + 0.99 * next_batch_action_values
 
         loss = self.loss_fn(batch_action_values, targets)
         self.optimizer.zero_grad()
@@ -91,38 +93,30 @@ class Agent:
 
         self.target_q_network.load_state_dict(target_dict)
 
-        return loss.item()
-
 
 if __name__ == '__main__':
-    env = gymnasium.make('CartPole-v1', render_mode="rgb_array")
+    env = gymnasium.make('CartPole-v1', render_mode="human")
     agent = Agent()
     memory = Memory()
-    for i_episode in trange(600):
-        observation, _ = env.reset()
-        for t in range(500):
-            action = agent.choose_action(observation)
-            observation_, reward, terminated, truncated, _ = env.step(action)
+    scores = deque([], maxlen=10)
+    with trange(600) as bar:
+        for i_episode in bar:
+            observation, _ = env.reset()
+            for t in range(500):
+                action = agent.choose_action(observation)
+                observation_, reward, terminated, truncated, _ = env.step(action)
 
-            # Store the transition in memory
-            memory.push(observation, action, reward, observation_, terminated)
+                # Store the transition in memory
+                memory.push(observation, action, reward, None if terminated else observation_)
 
-            # Perform one step of the optimization (on the policy network)
-            agent.train(memory)
+                # Perform one step of the optimization (on the policy network)
+                agent.train(memory)
 
-            # Move to the next state
-            observation = observation_
+                # Move to the next state
+                observation = observation_
 
-            if terminated or truncated:
-                break
-
-    env = gymnasium.make('CartPole-v1', render_mode="human")
-    for i_episode in range(100):
-        # Initialize the environment and get its state
-        observation, _ = env.reset()
-        for t in range(200):
-            action = agent.action(observation)
-            observation_, reward, terminated, truncated, _ = env.step(action)
-            if terminated or truncated:
-                break
-        print('Episode {} finished after {}', i_episode)
+                if terminated or truncated:
+                    scores.append(t)
+                    score = sum(scores) / len(scores)
+                    bar.set_description(f'score: {score}')
+                    break
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import collections
 import torch
 
 if __name__ == '__main__':
-    q = collections.deque(maxlen=10)
-    q.append([1, 2, 3])
-    q.append([4, 5, 6])
-    a = list(zip(*q))
-    print(a)
+    a = torch.tensor([[1, 2], [3, 4]], dtype=torch.float32)
+    b = a.max(1)[0]
+    print(b)
+    c = torch.zeros(10)
+    print(c)
